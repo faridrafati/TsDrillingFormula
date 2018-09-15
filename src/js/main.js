2bcb512bbbfb06915ef33f39b99f11f220b7dcb0
Mehran <mehran@mehran.com>
2018-09-15 13:40:44 +0430

    complete styles. next step is start formulas writing.

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,15 @@
-
 $(document).ready(function () {
     $("#tree_view").filetree({
         collapsed: true,
         animationSpeed: 'fast'
+    });
+
+    $(".click_formula").on('click', function () {
+        var htm_file = $(this).data('htm');
+        if (htm_file == undefined || htm_file == "") return;
+        var file_name = `./htm/${htm_file}`;
+
+        var frame = $("#panel_frame");
+        frame.attr('src', file_name);
     });
 });
